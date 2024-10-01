2c38b12c483b562de6ed274ca754dbb07cda38d9
Liam <liam.nguyen306@gmail.com>
2024-10-01 18:13:16 +0700

    add eth, bsc, polygon

--- src/utils/ethers.ts
+++ src/utils/ethers.ts
@@ -1,5 +1,14 @@
-import { C_CHAIN_RPC, SUBNET_RPC } from '$constants/contract';
+import {
+  BSC_RPC,
+  C_CHAIN_RPC,
+  ETH_RPC,
+  POLYGON_RPC,
+  SUBNET_RPC,
+} from '$constants/contract';
 import { ethers } from 'ethers';
 
 export const C_CHAIN_PROVIDER = new ethers.JsonRpcProvider(C_CHAIN_RPC);
 export const SUBNET_PROVIDER = new ethers.JsonRpcProvider(SUBNET_RPC);
+export const ETH_PROVIDER = new ethers.JsonRpcProvider(ETH_RPC);
+export const BSC_PROVIDER = new ethers.JsonRpcProvider(BSC_RPC);
+export const POLYGON_PROVIDER = new ethers.JsonRpcProvider(POLYGON_RPC);
--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -1,5 +1,14 @@
-import { C_CHAIN_RPC, SUBNET_RPC } from '$constants/contract';
+import {
+  BSC_RPC,
+  C_CHAIN_RPC,
+  ETH_RPC,
+  POLYGON_RPC,
+  SUBNET_RPC,
+} from '$constants/contract';
 import Web3 from 'web3';
 
 export const web3Subnet = new Web3(SUBNET_RPC);
 export const web3CChain = new Web3(C_CHAIN_RPC);
+export const web3Eth = new Web3(ETH_RPC);
+export const web3Bsc = new Web3(BSC_RPC);
+export const web3Polygon = new Web3(POLYGON_RPC);
