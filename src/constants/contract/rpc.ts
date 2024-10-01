45a461a4cdf10860489de946378f051c65829cda
Liam <liam.nguyen306@gmail.com>
2024-10-01 18:05:32 +0700

    add eth, bsc and polygon rpc

--- src/constants/contract/rpc.ts
+++ src/constants/contract/rpc.ts
@@ -1,2 +1,5 @@
-export const SUBNET_RPC = process.env.SUBNET_RPC;
-export const C_CHAIN_RPC = process.env.C_CHAIN_RPC;
+export const SUBNET_RPC = process.env.SUBNET_RPC!;
+export const C_CHAIN_RPC = process.env.C_CHAIN_RPC!;
+export const ETH_RPC = process.env.ETH_RPC!;
+export const BSC_RPC = process.env.BSC_RPC!;
+export const POLYGON_RPC = process.env.POLYGON_RPC!;
